652837c52255b4ca5b61ee190eb6562f55734b9e
yashpandey474 <ypandey474@gmail.com>
2023-07-02 17:53:59 +0530

    Add mongodb connection and blog creation with model

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
-
+//BLOG WEBSITE 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-var str = require('lodash/string');
+const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,8 +17,26 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//ARRAY FOR STORING ALL PUBLISHED POSTS
-posts = [];
+mongoose.connect("mongodb://127.0.0.1:27017/blogsDB");
+
+
+//CREATE SCHEMA FOR BLOG POSTS
+const blogSchema = new mongoose.Schema({
+  title:{
+    type: String,
+    required: True
+  },
+  content: {
+    type: String
+  }
+});
+
+//CREATE MODEL FOR BLOG POSTS COLLECTION
+const Blog = mongoose.model(
+  "Blog",
+  blogSchema
+);
+
 //1. ROUTE GET REQUESTS AT HOME
 app.get(
   "/",
@@ -53,9 +72,9 @@ app.get(
 app.get(
   "/posts/:post",
   function(request, response){
-    var requestedTitle = str.lowerCase(request.params.post);
+    var requestedTitle = _.lowerCase(request.params.post);
     posts.forEach(function(post){
-      var storedTitle = str.lowerCase(post.title)
+      var storedTitle = _.lowerCase(post.title)
       // CHECK FOR POST WITH SAME TITLE
       if (storedTitle == requestedTitle){
         response.render("post", {
@@ -70,13 +89,16 @@ app.get(
 app.post(
   "/compose",
   function(request, response){
-    const post = {
-      content: request.body.postContent,
-      title: request.body.postTitle
-    };
+    //CREATE NEW BLOG OBJECT
+    const blog = new Blog({
+      title: request.body.postTitle,
+      content: request.body.postContent
+    });
 
-    posts.push(post);
+    //SAVE THE BLOG
+    blog.save();
 
+    //SHOW BLOGS ON HOME PAGE
     response.redirect("/")
 
 });
